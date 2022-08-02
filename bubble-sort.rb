7cdb23b515126df829731d9d9493c00df5e562a9
ChewingTabasco <emerickjustice@gmail.com>
2022-08-02 16:47:56 -0400

    Rewrite logic using while loops

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,15 +1,33 @@
-def bubble_sort(array)
-	array.each.with_index do |current_num, index|
-		unless index == array.size - 1
-			next_num = array[index + 1]
+# def bubble_sort(array)
+# 	array.each.with_index do |current_num, index|
+# 		unless index == array.size - 1
+# 			next_num = array[index + 1]
 
-			if current_num > next_num
+# 			if current_num > next_num
+# 				array[index], array[index + 1] = array[index + 1], array[index]
+# 				bubble_sort(array)
+# 			end
+# 		end
+# 	end
+# end
+
+def bubble_sort(array)
+	unsorted = true
+
+	while unsorted do
+		index = 0
+		unsorted = false
+
+		while index < (array.size - 1)
+			if array[index] > array[index + 1]
 				array[index], array[index + 1] = array[index + 1], array[index]
-				bubble_sort(array)				
+				unsorted = true
+				p array
 			end
+			index += 1
 		end
 	end
-
+	array
 end
 
 
